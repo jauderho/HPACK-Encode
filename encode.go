2f63f2f6aaaaf96f1534e7e64fa3e52943b86924
Jauder Ho <jauderho@users.noreply.github.com>
2022-04-22 16:30:36 +0000

    Commit encode.go instead
    Signed-off-by: Jauder Ho <jauderho@users.noreply.github.com>

--- encode.go
+++ encode.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-    "fmt"
-    "golang.org/x/net/http2/hpack"
-    "os"
-    "strconv"
+	"fmt"
+	"golang.org/x/net/http2/hpack"
+	"os"
+	"strconv"
 )
 
 func main() {
-    fmt.Println(Encode(os.Args[1]))
+	fmt.Println(Encode(os.Args[1]))
 }
 
 func Encode(s string) string {
-    var result string
-    var count int
+	var result string
+	var count int
 
-    hd := hpack.AppendHuffmanString(nil, s)
-    hl := hpack.HuffmanEncodeLength(s) | 0x80
+	hd := hpack.AppendHuffmanString(nil, s)
+	hl := hpack.HuffmanEncodeLength(s) | 0x80
 
-    result += RenderByte(byte(hl))
+	result += RenderByte(byte(hl))
 
-    for _, b := range hd {
-        result += RenderByte(b)
-        count += 1
-    }
+	for _, b := range hd {
+		result += RenderByte(b)
+		count += 1
+	}
 
-    return "static const u_char nginx[" + strconv.Itoa(count + 1) + "] = \"" + string(result) + "\";"
+	return "static const u_char nginx[" + strconv.Itoa(count+1) + "] = \"" + string(result) + "\";"
 }
 
 func RenderByte(b byte) string {
-    return fmt.Sprintf("\\x%x", b)
+	return fmt.Sprintf("\\x%x", b)
 }
